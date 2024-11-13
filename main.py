4191bf43e98c9fb1088cf0ebe00205a4a8da648d
OnlyZalll <168963131+onlyzalll@users.noreply.github.com>
2024-11-14 03:09:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 
@@ -9,9 +10,19 @@ api_hash = os.getenv("API_HASH")
 
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
+OWNER_ID = 5854836745
+
 @app.on_message(filters.command("start"))
-def start(client, message):
+def _(client, message):
     client.send_message(message.chat.id, "Halo, saya bot!")
 
+@app.on_message(filters.command("leaveall"))
+def exit_groups(client, message):
+    if message.from_user.id == OWNER_ID:
+        for chat in client.get_chats():
+            if chat.type in ["supergroup", "channel"]:
+                client.leave_chat(chat.id)
+        client.send_message(message.chat.id, "Bot telah keluar dari semua grup dan channel!")
+        
 app.run()
-print("Bot berhasil login dan aktif!")
+        
